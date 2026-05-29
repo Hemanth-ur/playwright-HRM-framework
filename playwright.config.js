7f75f1316bce3f0a8ee05e0412de20c1125d0756
Hemanth <hemanthur15@gmail.com>
2026-05-29 08:20:54 +0530

    jenkis file added

--- playwright.config.js
+++ playwright.config.js
@@ -47,10 +47,6 @@ export default defineConfig({
         {
             name: 'chromium',
             use: { browserName: 'chromium' }
-        },
-        {
-            name: 'firefox',
-            use: { browserName: 'firefox' }
         }
     ]
 });
